6894f44971468013747a9a317f1a487eaf67f15d
kenechukwu <keneokekeag@gmail.com>
2026-06-20 21:23:27 +0100

    Add initial implementation of myFunction to compare two numbers

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+let myVar = 23n;
+ function myFunction(a,b){
+    if(a > b){
+        return "It is greater"
+    }
+    else if(a == b){
+        return"it is equal"
+    }
+    else return"it is less"
+ }
+
+console.log(myFunction(4,6))
