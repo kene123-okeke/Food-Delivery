25b18440a6638cd85511633e1d6b981f942dadb6
kenechukwu <keneokekeag@gmail.com>
2026-08-15 00:23:34 +0100

    feat: add close functionality for overlay panel in logout confirmation dialog

--- JS/index.js
+++ JS/index.js
@@ -2,7 +2,12 @@ const logoutButton = document.querySelectorAll('.login-btn');
 const logoutBox = document.getElementById('logout-box');
 const cancel = document.querySelectorAll('.cancel-btn');
 const overLay = document.getElementById('overlay');
+const overlayPanel = document.querySelectorAll('.overlay')
 
+function closeBox(){
+    logoutBox.style.display = "none";
+    overLay.style.display = "none";
+}
 logoutButton.forEach(btn => {
     btn.addEventListener('click', () =>{
         logoutBox.style.display = "block";
@@ -12,7 +17,11 @@ logoutButton.forEach(btn => {
             b.addEventListener('click', () => {
                 logoutBox.style.display = "none";
                 overLay.style.display = "none";
-            })
-        })
-    })
-})
+            });
+        });
+
+        overlayPanel.forEach(b => {
+            b.addEventListener('click', closeBox)
+        });
+    });
+});
