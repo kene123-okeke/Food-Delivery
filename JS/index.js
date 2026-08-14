aae626cecb95b8ad1757b29614c5244dd34606f8
kenechukwu <keneokekeag@gmail.com>
2026-08-15 00:02:32 +0100

    feat: implement logout confirmation dialog with overlay and cancel option

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,18 @@
-let myVar = 23n;
- function myFunction(a,b){
-    if(a > b){
-        return "It is greater"
-    }
-    else if(a == b){
-        return"it is equal"
-    }
-    else return"it is less"
- }
+const logoutButton = document.querySelectorAll('.login-btn');
+const logoutBox = document.getElementById('logout-box');
+const cancel = document.querySelectorAll('.cancel-btn');
+const overLay = document.getElementById('overlay');
 
-console.log(myFunction(4,6))
+logoutButton.forEach(btn => {
+    btn.addEventListener('click', () =>{
+        logoutBox.style.display = "block";
+        overLay.style.display = "flex";
+
+        cancel.forEach(b => {
+            b.addEventListener('click', () => {
+                logoutBox.style.display = "none";
+                overLay.style.display = "none";
+            })
+        })
+    })
+})
